65d1ef8ae9781f5a651dd74797f34547b6f7f102
daniilbelov <tseflutty@yandex.ru>
2022-12-19 21:52:56 +0300

    Add triangle

---
+++ SquareLibrary.Tests/TriangleTests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+
+namespace Splatrika.SquareLibrary.Tests;
+
+public class TriangleTests
+{
+    [Theory]
+    [InlineData(2, 2, 2.8284271247)]
+    [InlineData(3, 4, 5)]
+    public void CreateWithValidEdges(double a, double b, double c)
+    {
+        new Triangle(a, b, c);
+    }
+
+
+    [Theory]
+    [InlineData(2, 2, 2)]
+    [InlineData(3, 4, 5.5)]
+    public void CreateWithInvalidEdges(double a, double b, double c)
+    {
+        Assert.Throws<ArgumentException>(() => new Triangle(a, b, c));
+    }
+
+
+    [Theory]
+    [InlineData(2, 2, 2.8284271247, 2)]
+    [InlineData(3, 4, 5, 6)]
+    public void CalculateSquare(double a, double b, double c,
+        double exceptedSquare)
+    {
+        var triangle = new Triangle(a, b, c);
+        var square = triangle.CalculateSquare();
+
+        Assert.Equal(exceptedSquare, square, Constants.Precision);
+    }
+}
+
---
+++ SquareLibrary/Triangle.cs
@@ -0,0 +1,33 @@
+﻿using Splatrika.SquareLibrary.Abstractions;
+
+namespace Splatrika.SquareLibrary;
+
+public class Triangle : IShape
+{
+    public double A { get; }
+    public double B { get; }
+    public double C { get; }
+
+
+    public Triangle(double a, double b, double c)
+    {
+        var isRight =
+            Math.Round(a * a + b * b, Constants.Precision)
+            == Math.Round(c * c, Constants.Precision);
+        if (!isRight)
+        {
+            throw new ArgumentException("Must be a right triangle");
+        }
+
+        A = a;
+        B = b;
+        C = c;
+    }
+
+
+    public double CalculateSquare()
+    {
+        return A * B * 0.5;
+    }
+}
+
